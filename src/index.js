6f122b1b874eb44cc94a1eed50c0cceeb236f385
Kian James F. Querubin <kianjames.querubin.f@bulsu.edu.ph>
2023-04-03 15:23:48 +0800

    init dotenv & login.js in routes

--- src/index.js
+++ src/index.js
@@ -1,59 +1,32 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql");
 
 const app = express();
-//!Create Database Connection
+//*Create Database Connection
 
 const db = mysql.createConnection({
-	host: "sql12.freesqldatabase.com",
-	port: "3306",
-	user: "sql12609253",
-	password: "XarQS5eM1u",
-	database: "sql12609253",
+	host: process.env.DB_HOST,
+	port: process.env.DB_HOST_PORT,
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 });
 
-//!Connect
+//*Connect to Database
 
 db.connect((err) => {
 	if (err) {
 		console.log(err);
 	} else {
-		app.listen("3535", () => {
-			console.log("Server running on port 3535");
+		app.listen(process.env.PORT || "3535", () => {
+			console.log("Server running");
 		});
 		console.log("Application successfully connected to cms_db");
 	}
 });
 
-app.get("/", (req, res) => {
-	res.send("Homepage");
-});
+//*Routes
 
-app.get("/createTable", async (req, res) => {
-	try {
-		// Define SQL query to create table
-		const createTableQuery = `CREATE TABLE IF NOT EXISTS user_management (
-      id INT(11) NOT NULL AUTO_INCREMENT,
-      username VARCHAR(50) NOT NULL,
-      password VARCHAR(255) NOT NULL,
-      email VARCHAR(50) NOT NULL,
-      role VARCHAR(20) NOT NULL,
-      status ENUM('active', 'inactive') NOT NULL,
-      PRIMARY KEY (id)
-    )`;
-
-		// Execute query and handle response
-		db.query(createTableQuery, (err, result, fields) => {
-			if (err) {
-				console.error(err);
-				res.status(500).send(err);
-			} else {
-				console.log(result);
-				res.send(result);
-			}
-		});
-	} catch (err) {
-		console.error(err);
-		res.status(500).send(err);
-	}
-});
+const loginRoutes = require("./routes/login");
+app.use("/login", loginRoutes(db));
---
+++ src/routes/login.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+function loginRoutes(db) {
+	// *ROUTES GOES INSIDE HERE
+
+	return router;
+}
+
+module.exports = loginRoutes;
